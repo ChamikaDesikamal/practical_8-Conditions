e26eaee2f3e09a71943d02a3b6a315121308d41e
chamika <chamikadesikaal072@gmail.com>
2023-04-23 09:49:44 +0530

    You cn find Conditions Practicle here

---
+++ src/C.java
@@ -0,0 +1,34 @@
+public class C {
+    void plus(){
+        int a = 10;
+        long b= 123456;
+        long c = a+ b;
+        if (c<1000000| c == 123){
+            System.out.println(c+ " calling if");
+        }else {
+            System.out.println(b);
+        }
+    }
+}
+class D extends C{
+    void minus(){
+        double a = 123.876;
+        double b = 524237.954388;
+        double c = b- a ;
+        if (c > 500000 && c == 1524853.6456){
+            System.out.println(c);
+        }else if(c > 500000 && c < b ){
+            System.out.println(c + " calling else if");
+        }else {
+            System.out.println(b);
+        }
+    }
+
+    public static void main(String[] args) {
+        D d = new D();
+        C c = new C();
+
+        d.minus();
+        c.plus();
+    }
+}
